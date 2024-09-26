e762e667cd10033782134407a0144649042ddf87
Thomas Verboven <thomasverboven06@gmail.com>
2024-09-27 01:44:46 +0200

    final changes, tests gemaakt, comments toegevoegd, ...

--- helikopter.rb
+++ helikopter.rb
@@ -1,14 +1,14 @@
 class Helikopter
   RICHTINGEN = %w(noord oost zuid west)
 
-  def initialize(x, y, z, richting)
+  def initialize(x, y, z, richting)                                       # geeft de nieuwe instance van Helikoper zijn beginpositie en richting
     @x = x
     @y = y
     @z = z
     @richting = richting
   end
 
-  def vlieg(instructies)
+  def vlieg(instructies)                                                       # kijkt een voor een naar de instructies en voert de overeenkomstige beweging uit
     instructies.each_char do |char|
       case char
       when "R" then keer_naar_rechts
@@ -16,33 +16,33 @@ class Helikopter
       when "V" then ga_vooruit
       when "U" then ga_omhoog
       when "D" then ga_omlaag
-      else
+      else                                                                     # bij een ongeldige instructie wordt het programma afgebroken
         puts "#{char} is geen geldige instructie"
         exit
       end
     end
-    locatie
+    locatie                                                                    # returns eindlocatie
   end
 
-  private
+  private                                                                      # de volgende functies kunnen niet rechtstreeks opgeroepen worden buiten de klas
 
-  def keer_naar_rechts
+  def keer_naar_rechts                                                         # richt de helikopter naar de volgende windrichting met de klok mee
     richting_index = RICHTINGEN.index(@richting)
     @richting = RICHTINGEN[(richting_index + 1) % RICHTINGEN.length]
   end
 
-  def keer_naar_links
+  def keer_naar_links                                                          # richt de helikopter naar de volgende windrichting tegen de klok in
     richting_index = RICHTINGEN.index(@richting)
     @richting = RICHTINGEN[(richting_index - 1) % RICHTINGEN.length]
   end
 
-  def ga_vooruit
+  def ga_vooruit                                                               # kijkt naar waar de helikopter gericht staat en past het overeenkomsig coordinaat aan
     case @richting
     when "noord" then @y += 1
     when "oost" then @x += 1
     when "zuid" then @y -= 1
     when "west" then @x -= 1
-    else
+    else                                                                       # bij een ongeldige richting word het programma afgebroken
       puts "Geen geldige richting."
       exit
     end
@@ -56,22 +56,22 @@ class Helikopter
     @z -= 1
   end
 
-  def locatie
+  def locatie                                                                  # defenieerd hoe de eindlokatie weergegeven wordt
     "#{@x} #{@y} #{@z} #{@richting}"
   end
 end
 
-def check_input
+def check_input                                                                # kijkt als er genoeg argumenten gegeven zijn en zet deze in variable
   if ARGV.length == 5
     x, y, z, richting, instructies = ARGV[0..4]
     check_input_valid(x, y, z, richting, instructies)
-  else
+  else                                                                         # als dit niet het geval is gaat hij nieuwe input vragen
     puts "5 argumenten verwacht, #{ARGV.length} argument(en) gegeven."
     get_input
   end
 end
 
-def check_input_valid(x, y, z, richting, instructies)
+def check_input_valid(x, y, z, richting, instructies)                          # kijkt als de input voldoet aan de voorwaarden
   positie = [x, y, z]
   check_positie = positie.all? { |arg| arg =~ /^-?\d+$/ }
 
@@ -80,27 +80,28 @@ def check_input_valid(x, y, z, richting, instructies)
 
   check_instructies = instructies =~ /^[UDVRL]+$/
 
-  if check_positie && check_richting && check_instructies
+  if check_positie && check_richting && check_instructies                      # als alle inputs voldoen aan de voorwaarden returns hij de inputs
     [x, y, z, richting, instructies]
-  else
+  else                                                                         # als dit niet is gaat hij niewe inputs vragen
     puts "Argumenten voldoen niet aan verwachting."
     get_input
   end
 end
 
-def get_input
+def get_input                                                                  # vraagt voor nieuwe inputs als de vorige leeg/ongeldig waren
   puts "Input: ( <x> <y> <z> <richt> <instruct> )"
   input = gets.chomp
   if input.split(" ").length == 5
     x, y, z, richting, instructies = input.split(" ")
     check_input_valid(x, y, z, richting, instructies)
-  else
+  else                                                                         # als de nieuwe inputs leeg/ongeldig zijn vraagt hij er terug nieuwe
     puts "5 argumenten verwacht, #{input.split(" ").length} argument(en) gegeven."
+    get_input
   end
 
 end
 
 
-x, y, z, richting, instructies = check_input
-helikopter = Helikopter.new(x.to_i, y.to_i, z.to_i, richting)
-puts helikopter.vlieg(instructies)
+x, y, z, richting, instructies = check_input                              # neemt de nagekeken inputs en zet deze in de overeenkomstige variable
+helikopter = Helikopter.new(x.to_i, y.to_i, z.to_i, richting)             # maakt nieuwe instance van Helikopter met gegeven inputs
+puts helikopter.vlieg(instructies)                                        # instance van Helikopter voert vlieginstructies uit en print eindlocatie
--- spec/helikopter_spec.rb
+++ spec/helikopter_spec.rb
@@ -3,13 +3,57 @@ require 'spec_helper'
 require_relative '../helikopter'
 
 RSpec.describe Helikopter do
+
+  before do
+    @helikopter = Helikopter.new(0, 0, 0, "noord")
+  end
+
   describe "#initialize" do
-    it "initialiseert de helikopter met de juiste positie en richting" do
-      expect(helikopter.instance_variable_get(:@x)).to eq(0)
-      expect(helikopter.instance_variable_get(:@y)).to eq(0)
-      expect(helikopter.instance_variable_get(:@z)).to eq(0)
-      expect(helikopter.instance_variable_get(:@richting)).to eq("noord")
-      expect(helikopter.instance_variable_get(:@instructies)).to eq("RL")
+    it "initialiseert een instance van Helikopter met beginlocatie en richting" do
+      expect(@helikopter.instance_variable_get(:@x)).to eq(0)
+      expect(@helikopter.instance_variable_get(:@y)).to eq(0)
+      expect(@helikopter.instance_variable_get(:@z)).to eq(0)
+      expect(@helikopter.instance_variable_get(:@richting)).to eq("noord")
     end
+  end
+
+  describe "#vlieg" do
+    it "vliegt rechtdoor" do
+      @helikopter.vlieg("V")
+      expect(@helikopter.instance_variable_get(:@y)).to eq(1)
+    end
+    it "keert naar rechts"do
+      @helikopter.vlieg("R")
+      expect(@helikopter.instance_variable_get(:@richting)).to eq("oost")
+    end
+    it "keert naar links" do
+      @helikopter.vlieg("L")
+      expect(@helikopter.instance_variable_get(:@richting)).to eq("west")
+    end
+    it "vliegt omhoog" do
+      @helikopter.vlieg("O")
+      expect(@helikopter.instance_variable_get(:@z)).to eq(1)
+    end
+    it "vliegt omlaag" do
+      @helikopter.vlieg("D")
+      expect(@helikopter.instance_variable_get(:@z)).to eq(-1)
+    end
+    it "prints de eindlocatie van zijn vlucht" do
+      expect(@helikopter.vlieg("UURVVVLVVRRVD")).to eq("3, 3, 1 zuid")
+    end
+    it "breekt het programma af bij ongeldige instructies" do
+      expect(@helikopter.vlieg("vlieg naar leuven")).to raise_error(SystemExit)
+    end
+  end
+end
+
+describe "#check_input" do
+  it "returns de input als deze voldoet aan de voorwaarden" do
+    ARGV = ["0", "0", "0", "noord", "UURVVVLVVRVD"]
+    expect(check_input).to eq(["0", "0", "0", "noord", "UURVVVLVVRRVD"])
+  end
+  it "vraagt nieuwe input als deze voldoet aan de voorwaarden" do
+    ARGV = ["a", "b", "c", "noord"]
+    expect(check_input).to have_received(:get_input)
   end
 end
